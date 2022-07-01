b0be91e8664c991434e6210b0b4f1e0128029437
Paul Okoli <paulokoli7@gmail.com>
2022-07-01 10:35:39 -0700

    Adding 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	char e = 'e';
+	char q = 'q';
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != e && x != q)
+			{
+				putchar(x);
+			}
+	}
+	putchar('\n');
+	return (0);
+}
