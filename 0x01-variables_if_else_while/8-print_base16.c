31d15d7dbad3512983adc92a824bf597f8446b6d
Paul Okoli <paulokoli7@gmail.com>
2022-07-01 11:48:45 -0700

    Adding 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char x;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
