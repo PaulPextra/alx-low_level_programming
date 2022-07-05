b28fe162b83f81da5b159fb825e1ced5393d0da1
Paul Okoli <paulokoli7@gmail.com>
2022-07-05 12:44:07 -0700

    Adding 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,22 @@
  */
 int print_sign(int n)
 {
+	int value;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		value = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		value = 0;
 	}
 	else
 	{
 		_putchar('-');
+		value = -1;
 	}
-	return (0);
+	return (value);
 }
