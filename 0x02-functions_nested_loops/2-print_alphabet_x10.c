15bec421dfe58087d71f8f52e8e0221a8b23900a
Paul Okoli <paulokoli7@gmail.com>
2022-07-05 09:36:40 -0700

    Adding 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase multiply by 10
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
