7bc1156c512c5e81df7bc6a6d3d2cece592b321e
Paul Okoli <paulokoli7@gmail.com>
2022-07-05 07:57:30 -0700

    Adding 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,5 +8,14 @@
 int main(void)
 {
 	_putchar('c');
+	_putchar('h');
+	_putchar('i');
+	_putchar('b');
+	_putchar('u');
+	_putchar('i');
+	_putchar('k');
+	_putchar('e');
+	_putchar('m');
+	_putchar('\n');
 	return (0);
 }
