eeff8fde0e64446e9810997c095ee3bf50674245
Paul Okoli <paulokoli7@gmail.com>
2022-07-05 15:53:25 -0700

    Adding 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a - parameter of int type
+ * @b - parameter of int type
+ *
+ * Return: Result of addition of both numbers
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
