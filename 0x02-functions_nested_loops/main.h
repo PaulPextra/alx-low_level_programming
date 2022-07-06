c45a303d3ab89c006e591c8dea7c17c6a8ddf0fe
Paul Okoli <paulokoli7@gmail.com>
2022-07-06 13:09:44 -0700

    Adding main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -16,6 +15,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void putformat(int n);
-void positive_or_negative(int i);
 
 #endif
