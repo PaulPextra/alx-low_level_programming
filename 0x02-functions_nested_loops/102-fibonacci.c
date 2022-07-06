4dff8abbfa53da881367c561e5c32393f8395846
Paul Okoli <paulokoli7@gmail.com>
2022-07-06 04:17:52 -0700

    Adding 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,6 @@ int main(void)
 
 	fibonacci[0] = 0;
 	fibonacci[1] = 1;
-	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
