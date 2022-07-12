b42ba5041467a02396cea27b9f6e1a521df11b5a
Paul Okoli <paulokoli7@gmail.com>
2022-07-12 13:05:04 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: The string to be printed
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
