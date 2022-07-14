ab1b47cfeb0aae974f6ad41cdb6110eb5106a6b6
Paul Okoli <paulokoli7@gmail.com>
2022-07-14 10:16:00 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: The encoded string.
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i  = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+
+	return (str);
+}
