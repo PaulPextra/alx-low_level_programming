e41eff83604f5d954d0cced1d0ee8cd01ccbae0f
Paul Okoli <paulokoli7@gmail.com>
2022-08-01 06:42:37 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cpyname, *cpyowner;
 	int len_name = 0, len_owner = 0, i;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	while (name[len_name])
 		len_name++;
