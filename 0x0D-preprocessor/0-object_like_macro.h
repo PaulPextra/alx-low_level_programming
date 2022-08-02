cd759a7fa958074765968397811b139dd80ac6e7
Paul Okoli <paulokoli7@gmail.com>
2022-08-02 05:55:05 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO_H_
-#define _OBJECT_LIKE_MACRO_H_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* _OBJECT_LIKE_MACRO_ */
+#endif /* OBJECT_LIKE_MACRO */
